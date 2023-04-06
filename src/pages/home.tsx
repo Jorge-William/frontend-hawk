dcef91f4f7d59562320b4e84d5895f4ec4a2c339
Jorge William <jorge_william_cardoso@yahoo.com.br>
2023-04-06 20:54:50 -0300

    Feat: Envio inicial

---
+++ src/pages/home.tsx
@@ -0,0 +1,13 @@
+import logo from '../assets/logo-hawk.png'
+
+const Home = () => {
+    return (
+        <div>
+            <header id='divider'><img src={logo} alt="Logo da empresa Hawk" id='logo'/></header>
+            <h1 className='titulo'>PRONTUÁRIO - HAWK SEGURANÇA E VIGILÂNCIA LTDA</h1>
+        </div>
+
+    )
+} 
+
+export default Home
